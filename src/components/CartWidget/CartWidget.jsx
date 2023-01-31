0354b6aa77982d5c857f96e973c055e05d30df8a
unknown <cora.gagliardi@gmail.com>
2023-01-31 18:43:37 +1000

    corrigiendo rutas relativas

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const CartWidget = ({cantidadCarrito}) => {
   return (
     <>
-      <button className="btn nav-link" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><img src="public/images/iconoCarrito.png" alt="icono carrito" id="icono_carrito" width={20} height={20} /></button>
+      <button className="btn nav-link" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><img src="../../public/images/iconoCarrito.png" alt="icono carrito" id="icono_carrito" width={20} height={20} /></button>
       <p>{cantidadCarrito}</p>
     </>
   )
